e11f572626f1b40bcbda38a4ae491e6e9c311367
Alex Snaps <alex@wcgw.dev>
2022-06-03 17:00:42 -0400

    Added return_to logic to ConcurrentCounter

--- limitador/src/storage/concurrent.rs
+++ limitador/src/storage/concurrent.rs
@@ -4,7 +4,7 @@ use crate::{Authorization, Counter, Limit, Namespace, Storage};
 use std::collections::hash_map::Entry;
 use std::collections::{HashMap, HashSet};
 use std::sync::atomic::{AtomicU64, Ordering};
-use std::sync::{LockResult, RwLock};
+use std::sync::RwLock;
 
 pub struct ConcurrentStorage {
     data: RwLock<HashMap<String, ConcurrentNamespace>>,
@@ -116,7 +116,7 @@ impl Default for ConcurrentStorage {
 #[derive(Eq, Hash, PartialEq)]
 struct ConcurrentNamespace {
     name: String,
-    limits: HashSet<ConcurrentLimit>,
+    // limits: HashSet<ConcurrentLimit>,
 }
 
 impl ConcurrentNamespace {
@@ -131,11 +131,12 @@ impl From<String> for ConcurrentNamespace {
     fn from(name: String) -> Self {
         Self {
             name,
-            limits: HashSet::new(),
+            // limits: HashSet::new(),
         }
     }
 }
 
+#[derive(Eq, Hash, PartialEq)]
 enum ConcurrentLimit {
     SimpleLimit,
     QualifiedLimit,
@@ -160,24 +161,52 @@ pub struct ConcurrentCounter {
 }
 
 impl ConcurrentCounter {
-    pub fn next_at(&self, at: u64) -> u64 {
-        let expiry = self.expires_at.load(Ordering::Acquire);
+    pub fn next_at(&self, at: u64, delta: u64) -> (u64, u64) {
+        let mut expiry = self.expires_at.load(Ordering::Acquire);
         if at >= expiry {
-            if self
-                .expires_at
-                .compare_exchange(
-                    expiry,
-                    at + self.duration,
-                    Ordering::Release,
-                    Ordering::Relaxed,
-                )
-                .is_ok()
-            {
-                self.value.store(1, Ordering::Release);
-                return 1;
+            match self.expires_at.compare_exchange(
+                expiry,
+                at + self.duration,
+                Ordering::Release,
+                Ordering::Relaxed,
+            ) {
+                Ok(expiry) => {
+                    self.value.store(delta, Ordering::Release);
+                    return (delta, expiry);
+                }
+                Err(updated) => expiry = updated,
             }
         }
-        self.value.fetch_add(1, Ordering::Relaxed) + 1
+        (
+            self.value.fetch_add(delta, Ordering::Relaxed) + delta,
+            expiry,
+        )
+    }
+
+    pub fn return_to(&self, at: u64, delta: u64) -> bool {
+        let nuclear = Ordering::SeqCst;
+        let mut current = self.value.load(nuclear);
+        // are we still in time to return?
+        while at == self.expires_at.load(nuclear) {
+            // we are, can we even subtract that value without rolling over?
+            if current > delta {
+                // ok, let's try
+                match self
+                    .value
+                    .compare_exchange(current, current - delta, nuclear, nuclear)
+                {
+                    Ok(_) => return true,
+                    Err(updated) => current = updated,
+                }
+                // otherwise... spin. Tho this could spin until the counter expires!
+                // might want to just tableflip after a few tries
+            } else {
+                // no? either we expired or just wrong delta
+                return false;
+            }
+        }
+        // we (finally?) expired
+        false
     }
 }
 
